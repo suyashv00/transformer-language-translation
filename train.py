964429bbc37262b5daeb51d7b50e26877b34bba4
suyashv00 <suyashvetal01@gmail.com>
2026-08-19 11:45:48 +0530

    Added tokenizer logic

---
+++ train.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, random_split
+
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+from pathlib import Path
+
+def get_all_sentences(ds, lang):
+    for item in ds:
+        yield item[lang]
+
+def get_or_build_tokenizer(config, ds, lang):
+    tokenizer_path = Path(config['tokenizer_file'].format(lang))
+    if not Path.exists(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token=['UNK']))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    
+    return tokenizer
+
+def get_ds(config):
+    ds_raw = load_dataset('anujsahani01/English-Marathi', split='train')
+
+    # Use only 30,000 rows
+    ds_raw = ds_raw.select(range(30000))
+    
+    # Build Tokenizers 
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
+    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
+
+    # Keep 90% data for training and 10% for validation 
+    train_ds_size = int(0.9 * len(ds_raw))
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
